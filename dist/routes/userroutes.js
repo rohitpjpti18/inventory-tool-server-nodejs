a8e5132e3e95e9a28fb60190f07450e4c094585c
Rohit Prajapati <51499109+rohitpjpti18@users.noreply.github.com>
2021-09-09 09:47:19 +0530

    remove dist

--- dist/routes/userroutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var userroutes = express_1.default.Router();
-userroutes.get('/register', function (req, res, next) {
-});
